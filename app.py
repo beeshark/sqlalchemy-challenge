d7209df118a89206bb61179bdb229e1d28c57182
Sara <sbehnke33@yahoo.com>
2020-10-13 21:29:07 -0500

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+#code starts here
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+import datetime as dt
+from datetime import timedelta
+
+# database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
+
+# flask routes
